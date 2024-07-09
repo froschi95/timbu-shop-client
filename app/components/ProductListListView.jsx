574e91175c7976b38f5729eb9130294dbef5edff
Frank Success <franksuccess95@gmail.com>
2024-07-10 00:08:04 +0100

    Update list view

--- app/components/ProductListListView.jsx
+++ app/components/ProductListListView.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import ProductCard from "./ProductCard";
 import Image from "next/image";
 import Star from "/public/Star 1.svg";
+import Heart from "/public/Heart.svg";
+import Compare from "/public/compare.svg";
 
 const ProductListListView = ({ products }) => {
   return (
-    <div className="relative flex flex-col w-full md:w-2/3 gap-4">
+    <div className="relative flex flex-col w-full md:w-2/3 gap-3">
       {products.map((product) => (
         <div key={product.id} className="relative flex gap-3">
           <div className="w-full">
             <ProductCard product={product} />
           </div>
           <div className="w-full">
-            <div className="flex flex-col">
+            <div className="flex flex-col gap-[11px]">
               <div className="text-sm">
                 <h1 className="font-bold">ZARA OFFICIAL</h1>
                 <h3 className="font-medium">{product.name}</h3>
@@ -21,6 +23,24 @@ const ProductListListView = ({ products }) => {
               <div className="flex">
                 <Image src={Star} alt="star" />
                 <p>4.8</p>
+              </div>
+              <div className="flex justify-between items-center">
+                <div className="flex gap-[6px] text-center text-xs">
+                  <p>Size</p>
+                  <div className="border-2 border-[#C4C4C4] w-6 h-6 rounded-full text-center text-xs">
+                    <p>S</p>
+                  </div>
+                  <div className="border-2 border-[#C4C4C4] w-6 h-6 rounded-full text-center text-xs">
+                    <p>M</p>
+                  </div>
+                  <div className="border-2 border-[#C4C4C4] w-6 h-6 rounded-full text-center text-xs">
+                    <p>L</p>
+                  </div>
+                </div>
+                <div className="flex gap-1">
+                  <Image src={Compare} alt="compare" />
+                  <Image src={Heart} alt="heart" />
+                </div>
               </div>
             </div>
           </div>
