c7954d6efd0914b8468a9efd3761716f79e5dcd9
Frank Success <franksuccess95@gmail.com>
2024-07-10 01:29:58 +0100

    Add Cart page

---
+++ app/cart/page.js
@@ -0,0 +1,76 @@
+import React from "react";
+import ProductCard from "../components/ProductCard";
+import Image from "next/image";
+import Star from "/public/Star 1.svg";
+import products from "../data/products.json";
+import Bag from "/public/bag.svg";
+
+const CartPage = () => {
+  return (
+    <div className="mx-auto px-6">
+      <div className="relative flex flex-col w-full md:w-2/3 gap-3">
+        {products.slice(1, 3).map((product) => (
+          <>
+            <div key={product.id} className="relative flex gap-3">
+              <div className="w-full">
+                <ProductCard product={product} />
+              </div>
+              <div className="w-full">
+                <div className="flex flex-col gap-[11px]">
+                  <div className="text-sm">
+                    <h1 className="font-bold">ZARA OFFICIAL</h1>
+                    <h3 className="font-medium">{product.name}</h3>
+                    {/* <p className="text-gray-600">{product.description}</p> */}
+                  </div>
+                  <div className="flex">
+                    <Image src={Star} alt="star" />
+                    <p>4.8</p>
+                  </div>
+
+                  <div className="flex gap-[6px] text-center text-xs">
+                    <p>Size</p>
+                    <div className="border-2 border-[#C4C4C4] bg-[#c4c4c441] w-6 h-6 rounded-full text-center text-xs">
+                      <p>S</p>
+                    </div>
+                    <div className="border-2 border-[#C4C4C4] w-6 h-6 rounded-full text-center text-xs">
+                      <p>M</p>
+                    </div>
+                    <div className="border-2 border-[#C4C4C4] w-6 h-6 rounded-full text-center text-xs">
+                      <p>L</p>
+                    </div>
+                  </div>
+                  <div className="flex gap-1 text-sm text-center">
+                    <div className="rounded-full w-6 h-6 border-2 border-[#C4C4C4]">
+                      <h2>-</h2>
+                    </div>
+                    <div className="rounded-full w-6 h-6 border-2 border-[#C4C4C4]">
+                      <h2>1</h2>
+                    </div>
+                    <div className="rounded-full w-6 h-6 border-2 border-[#C4C4C4]">
+                      <h2>+</h2>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <hr className="divide-y divide-[#C4C4C4]" />
+          </>
+        ))}
+        <div className="mt-20 h-11">
+          <hr className="divide-y divide-[#858585]" />
+
+          <div className="flex py-4 justify-between items-center">
+            <h1 className="text-sm font-bold">SUB TOTAL</h1>
+            <p className="text-sm font-bold text-[#A17E6D]">$299</p>
+          </div>
+        </div>
+        <div className="h-14 bg-black flex gap-6 justify-center items-center">
+          <Image src={Bag} alt="cart" />
+          <p className="text-sm font-bold text-white">BUY NOW</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartPage;
--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-gray-800 text-white p-4 text-center">
+    <footer className="mt-60 bg-gray-800 text-white p-4 text-center">
       <p>&copy; 2024 Timbu Cloud Shop. All rights reserved.</p>
     </footer>
   );
